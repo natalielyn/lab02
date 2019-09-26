376b6c47c16d8b5762db68b833c10e2b9d3aeed7
Holly Davis <hollydavis541@gmail.com>
2019-09-25 20:20:39 -0700

    variables and for loop creted for quiz part 7--guessing items in an array

--- js/app.js
+++ js/app.js
@@ -1,56 +1,62 @@
 'use strict';
 
-// var userName = prompt('Hi there, what\'s your name?');
-// //console.log('User\'s name: ' + userName);
-// alert('Hi ' + userName + '! I\'m going to ask you a few questions. Give it your best guess and then I\'ll let you know the answer.');
+var rightAnswer = 0;
 
-// var question01 = prompt('Do I have children?');
-// if (question01.toLowerCase() === 'no' || question01.toLowerCase() === 'n') {
-// //console.log('Correct! I do not have any kids, but am a proud aunt!');
-//   alert('Correct! I do not have any kids, but am a proud aunt!');
-// } else {
-// //console.log('Good guess, but I actually do not have any children. I am a proud aunt!');
-//   alert('Good guess, but I actually do not have any children. I am a proud aunt!');
-// }
+var userName = prompt('Hi there, what\'s your name?');
+//console.log('User\'s name: ' + userName);
+alert('Hi ' + userName + '! I\'m going to ask you a few questions. Give it your best guess and then I\'ll let you know the answer.');
 
-// var question02 = prompt('Is my favorite outdoor activity running?');
-// if (question02.toLowerCase() === 'no' || question02.toLowerCase() === 'n') {
-// //console.log('Correct! Running is NOT my jam. I LOVE hiking though and I\'ll wave to the hardcore trail runners as they pass me by.');
-//   alert('Correct! Running is NOT my jam. I LOVE hiking though and I\'ll wave to the hardcore trail runners as they pass me by.');
-// } else {
-// //console.log('Good guess, but running is NOT my jam. I LOVE hiking though and I\'ll wave to the hardcore trail runners as they pass me by.');
-//   alert('Good guess, but running is NOT my jam. I LOVE hiking though and I\'ll wave to the hardcore trail runners as they pass me by.');
-// }
+var question01 = prompt('Do I have children?');
+if (question01.toLowerCase() === 'no' || question01.toLowerCase() === 'n') {
+//console.log('Correct! I do not have any kids, but am a proud aunt!');
+  alert('Correct! I do not have any kids, but am a proud aunt!');
+  rightAnswer++;
+} else {
+//console.log('Good guess, but I actually do not have any children. I am a proud aunt!');
+  alert('Good guess, but I actually do not have any children. I am a proud aunt!');
+}
 
-// var question03 = prompt('Is my primary goal in life to work hard in order to spoil my future dog?');
-// if (question03.toLowerCase() === 'yes' || question03.toLowerCase() === 'y') {
-// //console.log('Correct! I love dogs (and most all other animals) and can\'t wait to have one or more to spoil.');
-//   alert('Correct! I love dogs (and most all other animals) and can\'t wait to have one or more to spoil.');
-// } else {
-// //console.log('Good guess, but I love dogs (and most all other animals) and can\'t wait to have one or more to spoil.');
-//   alert('Good guess, but I love dogs (and most all other animals) and can\'t wait to have one or more to spoil.');
-// }
+var question02 = prompt('Is my favorite outdoor activity running?');
+if (question02.toLowerCase() === 'no' || question02.toLowerCase() === 'n') {
+//console.log('Correct! Running is NOT my jam. I LOVE hiking though and I\'ll wave to the hardcore trail runners as they pass me by.');
+  alert('Correct! Running is NOT my jam. I LOVE hiking though and I\'ll wave to the hardcore trail runners as they pass me by.');
+  rightAnswer++;
+} else {
+//console.log('Good guess, but running is NOT my jam. I LOVE hiking though and I\'ll wave to the hardcore trail runners as they pass me by.');
+  alert('Good guess, but running is NOT my jam. I LOVE hiking though and I\'ll wave to the hardcore trail runners as they pass me by.');
+}
 
-// var question04 = prompt('Do I like cilantro?');
-// if (question04.toLowerCase() === 'no' || question04.toLowerCase() === 'n') {
-// //console.log('Correct! Cilantro tastes like soap to me (apparently this is a genetic thing). Bummer since it\'s common in so many great dishes.');
-//   alert('Correct! Cilantro tastes like soap to me (apparently this is a genetic thing). Bummer since it\'s common in so many great dishes.');
-// } else {
-// //console.log('Good guess, but cilantro tastes like soap to me (apparently this is a genetic thing). Bummer since it\'s common in so many great dishes.');
-//   alert('Good guess, but cilantro tastes like soap to me (apparently this is a genetic thing). Bummer since it\'s common in so many great dishes.');
-// }
+var question03 = prompt('Is my primary goal in life to work hard in order to spoil my future dog?');
+if (question03.toLowerCase() === 'yes' || question03.toLowerCase() === 'y') {
+//console.log('Correct! I love dogs (and most all other animals) and can\'t wait to have one or more to spoil.');
+  alert('Correct! I love dogs (and most all other animals) and can\'t wait to have one or more to spoil.');
+  rightAnswer++;
+} else {
+//console.log('Good guess, but I love dogs (and most all other animals) and can\'t wait to have one or more to spoil.');
+  alert('Good guess, but I love dogs (and most all other animals) and can\'t wait to have one or more to spoil.');
+}
 
-// var question05 = prompt('Is my current favorite TV series Mr. Robot?');
-// if (question05.toLowerCase() === 'yes' || question05.toLowerCase() === 'y') {
-// //console.log('Correct! Mr. Robot is an awesome show. Why aren\'t you watching it right now?');
-//   alert('Correct! Mr. Robot is an awesome show. Why aren\'t you watching it right now?');
-// } else {
-// //console.log('Good guess, but Mr. Robot is an awesome show. Why aren\'t you watching it right now?');
-//   alert('Good guess, but Mr. Robot is an awesome show. Why aren\'t you watching it right now?');
-// }
+var question04 = prompt('Do I like cilantro?');
+if (question04.toLowerCase() === 'no' || question04.toLowerCase() === 'n') {
+//console.log('Correct! Cilantro tastes like soap to me (apparently this is a genetic thing). Bummer since it\'s common in so many great dishes.');
+  alert('Correct! Cilantro tastes like soap to me (apparently this is a genetic thing). Bummer since it\'s common in so many great dishes.');
+  rightAnswer++;
+} else {
+//console.log('Good guess, but cilantro tastes like soap to me (apparently this is a genetic thing). Bummer since it\'s common in so many great dishes.');
+  alert('Good guess, but cilantro tastes like soap to me (apparently this is a genetic thing). Bummer since it\'s common in so many great dishes.');
+}
 
-// alert('Thanks, ' + userName + ', for learning more about me!');
+var question05 = prompt('Is my current favorite TV series Mr. Robot?');
+if (question05.toLowerCase() === 'yes' || question05.toLowerCase() === 'y') {
+//console.log('Correct! Mr. Robot is an awesome show. Why aren\'t you watching it right now?');
+  alert('Correct! Mr. Robot is an awesome show. Why aren\'t you watching it right now?');
+  rightAnswer++;
+} else {
+//console.log('Good guess, but Mr. Robot is an awesome show. Why aren\'t you watching it right now?');
+  alert('Good guess, but Mr. Robot is an awesome show. Why aren\'t you watching it right now?');
+}
 
+alert('Thanks, ' + userName + ', for learning more about me!');
 
 //the number the user needs to guess to be correct
 var correctNum = 17;
@@ -68,16 +74,35 @@ for (i = 0; i < 3; i++) {
     console.log(numGuess);
   } else if (numGuess === 17) {
     alert('You guessed right!');
+    console.log(numGuess);
+    rightAnswer++;
     break;
     //if someone enters something other than a number
   } else {
     numGuess = parseInt(prompt('Please enter a number:'));
+    console.log(numGuess);
   }
 }
 
 alert('The secret number is 17');
 
-// * Add a 6th question to the guessing game that takes in a numeric input by prompting the user to guess a number.
-// * Indicates through an alert if the guess is “too high” or “too low”.
-// * It should give the user exactly four opportunities to get the correct answer.
-// * After all attempts have been exhausted, tell the user the correct answer. Consider using a loop of some sort.
+// Quiz Part 07 - Guessing My Fav Animals
+var myFavAnimals = ['cows', 'pigs', 'dogs', 'hedgehogs', 'rockchucks'];
+// changed the returned value of the following variable to lowercase in order to match array values above
+var animalGuess = (prompt('Try guessing my favorite animals. Write your guess in plural format (e.g. "squirrels" which are awesome tree-friends, but not on my list for this game)')).toLowerCase();
+// console.log(animalGuess);
+// "i" stands for the user's current try number
+for (i = 0; i < 5; i++) {
+  // I found the "includes" method shown below at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+  if (!myFavAnimals.includes(animalGuess)) {
+    animalGuess = prompt('This is not one of my favorite animals. Try guessing again:');
+  // console.log(animalGuess);
+  } else {
+    animalGuess = prompt('Nice! This is on of my fav animals. Try guessing another one:');
+    // console.log(animalGuess);
+    rightAnswer++;
+  }
+}
+alert ('My favorite animals are cows, pigs, dogs, hedgehogs, and rockchucks.')
+
+alert('Thanks for taking my quiz! Nice job, ' + userName + ', you got ' + rightAnswer + ' questions correct!');
